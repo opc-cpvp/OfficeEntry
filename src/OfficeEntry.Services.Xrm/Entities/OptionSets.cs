24d0aed68f3b13643b2413161f356ddb6b5386e3
Pierluc Simplicio-Soucy <Pierluc.Simplicio-Soucy@priv.gc.ca>
2020-06-16 15:28:44 -0400

    merge

---
+++ src/OfficeEntry.Services.Xrm/Entities/OptionSets.cs
@@ -0,0 +1,19 @@
+﻿namespace OfficeEntry.Services.Xrm.Entities
+{
+    public enum AccessReasons
+    {
+        CriticalWork = 948160000,
+        RegularWork = 948160002,
+        PickUpADocument = 948160001,
+        PickUpOfficeEquipment = 948160003,
+        Other = 948160004
+    }
+
+    public enum ApprovalStatus
+    {
+        Pending = 948160000,
+        Approved = 948160001,
+        Declined = 948160002,
+        Cancelled = 948160003
+    }
+}
