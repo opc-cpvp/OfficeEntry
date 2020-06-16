b09ba50eb3610b31e6a5153429264f5b342a5cc0
Pierluc Simplicio-Soucy <Pierluc.Simplicio-Soucy@priv.gc.ca>
2020-06-16 15:28:44 -0400

    merge

---
+++ src/OfficeEntry.API/Controllers/AccessRequestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OfficeEntry.Domain.Contracts;
+
+namespace OfficeEntry.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccessRequestController : ControllerBase
+    {
+        private readonly IAccessRequestService _accessRequestService;
+
+        public AccessRequestController(IAccessRequestService accessRequestService)
+        {
+            _accessRequestService = accessRequestService;
+        }
+
+        [HttpPatch]
+        [Route("{accessRequestId}/approve")]
+        public Task Approve(Guid accessRequestId)
+        {
+             return _accessRequestService.ApproveAccessRequestAsync(accessRequestId);
+        }
+    }
+}
