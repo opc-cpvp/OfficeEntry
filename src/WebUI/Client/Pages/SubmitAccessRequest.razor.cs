80fc3ed87c92944c00caa2ad299cc5277519c2df
Joshua Campbell <joshcampbell191@gmail.com>
2020-06-22 16:27:25 -0400

    Added submit access request page

---
+++ src/WebUI/Client/Pages/SubmitAccessRequest.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OfficeEntry.WebUI.Client.Pages
+{
+    public abstract class SubmitAccessRequestBase : ComponentBase
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        public async Task OnSurveyCompleted(string surveyResult)
+        {
+        }
+    }
+}
