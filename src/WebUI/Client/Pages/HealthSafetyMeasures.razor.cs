acd2ef5a5671f9637433c81ea3673d9cfe14c16b
Pier-Luc Bonneville <pier-luc.bonneville@priv.gc.ca>
2020-06-22 10:38:30 -0400

    Code refactoring.

--- src/WebUI/Client/Pages/HealthSafetyMeasures.razor.cs
+++ src/WebUI/Client/Pages/HealthSafetyMeasures.razor.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace OfficeEntry.WebUI.Client.Pages
@@ -13,16 +14,32 @@ namespace OfficeEntry.WebUI.Client.Pages
         [Inject]
         public HttpClient Http { get; set; }
 
+        protected string SurveyData { get; set; }
+
         public async Task OnSurveyCompleted(string surveyResult)
         {
-            var surveyData = System.Text.Json.JsonSerializer.Deserialize<Rootobject>(surveyResult);
+            var surveyData = JsonSerializer.Deserialize<HealthAndSafetyMeasuresStatementSurveyData>(surveyResult);
 
             bool healthAndSafetyMeasuresAccepted = surveyData.questionAcceptHsmStatement.Any();
 
             await Http.PutAsJsonAsync("api/HealthAndSafetyMeasures", healthAndSafetyMeasuresAccepted);
         }
 
-        private protected class Rootobject
+        protected override async Task OnInitializedAsync()
+        {
+            var isPrivacyActStatementAccepted = await Http.GetFromJsonAsync<bool>("api/HealthAndSafetyMeasures");
+
+            var surveyData = new HealthAndSafetyMeasuresStatementSurveyData
+            {
+                questionAcceptHsmStatement = isPrivacyActStatementAccepted
+                    ? new string[] { "iAcceptHsmStatement" }
+                    : new string[0]
+            };
+
+            SurveyData = JsonSerializer.Serialize(surveyData);
+        }
+
+        private protected class HealthAndSafetyMeasuresStatementSurveyData
         {
             public string[] questionAcceptHsmStatement { get; set; }
         }
