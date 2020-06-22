3f3ba5d6b204235f137d74910544eb5ceb79a45a
Pier-Luc Bonneville <pier-luc.bonneville@priv.gc.ca>
2020-06-21 22:06:52 -0400

    Code refactoring.

--- src/WebUI/Client/Pages/PrivacyActStatement.razor.cs
+++ src/WebUI/Client/Pages/PrivacyActStatement.razor.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace OfficeEntry.WebUI.Client.Pages
@@ -13,16 +14,32 @@ namespace OfficeEntry.WebUI.Client.Pages
         [Inject]
         public HttpClient Http { get; set; }
 
+        protected string SurveyData { get; set; }
+
         public async Task OnSurveyCompleted(string surveyResult)
         {
-            var surveyData = System.Text.Json.JsonSerializer.Deserialize<Rootobject>(surveyResult);
+            var surveyData = JsonSerializer.Deserialize<PrivacyActStatementSurveyData>(surveyResult);
 
             bool privateActStatementAccepted = surveyData.questionAcceptPaStatement.Any();
 
             await Http.PutAsJsonAsync("api/PrivacyStatement", privateActStatementAccepted);
         }
 
-        private protected class Rootobject
+        protected override async Task OnInitializedAsync()
+        {
+            var isPrivacyActStatementAccepted = await Http.GetFromJsonAsync<bool>("api/PrivacyStatement");
+
+            var surveyData = new PrivacyActStatementSurveyData
+            {
+                questionAcceptPaStatement = isPrivacyActStatementAccepted
+                    ? new string[] { "iAcceptPaStatement" }
+                    : new string[0]
+            };
+
+            SurveyData = JsonSerializer.Serialize(surveyData);
+        }
+
+        private protected class PrivacyActStatementSurveyData
         {
             public string[] questionAcceptPaStatement { get; set; }
         }
