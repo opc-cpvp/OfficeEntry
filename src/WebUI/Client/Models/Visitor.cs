c4cff63370668261377c0c91b17f05c1bf57ed92
Joshua Campbell <joshcampbell191@gmail.com>
2020-06-23 16:18:30 -0400

    Added code to process the submission

---
+++ src/WebUI/Client/Models/Visitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficeEntry.WebUI.Client.Models
+{
+    public class Visitor
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string telephone { get; set; }
+    }
+}
--- src/WebUI/Client/Pages/SubmitAccessRequest.razor.cs
+++ src/WebUI/Client/Pages/SubmitAccessRequest.razor.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
+using OfficeEntry.WebUI.Client.Models;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OfficeEntry.Domain.Entities;
 
 namespace OfficeEntry.WebUI.Client.Pages
 {
@@ -11,6 +17,21 @@ namespace OfficeEntry.WebUI.Client.Pages
 
         public async Task OnSurveyCompleted(string surveyResult)
         {
+            var submission = JsonConvert.DeserializeObject<AccessRequestSubmission>(surveyResult);
+
+            var accessRequest = new AccessRequest
+            {
+                Building = new Building { Id = submission.building },
+                Details = submission.details,
+                EndTime = submission.startDate.AddHours(submission.endTime),
+                Floor = new Floor { Id = submission.floor },
+                Manager = new Contact { Id = submission.manager },
+                Reason = new OptionSet
+                {
+                    Key = submission.reason
+                },
+                StartTime = submission.startDate.AddHours(submission.startTime)
+            };
         }
     }
 }
