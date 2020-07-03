c97b1fd0488e53ad5d3edaa0c81f544a2aea7de9
Pier-Luc Bonneville <pier-luc.bonneville@priv.gc.ca>
2020-07-03 09:26:05 -0400

    Moved the NavMenu back into the Shared directory.

--- src/WebUI/Client/Shared/NavMenu.razor.cs
+++ src/WebUI/Client/Shared/NavMenu.razor.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OfficeEntry.WebUI.Client.Shared.NavMenu
+namespace OfficeEntry.WebUI.Client.Shared
 {
     public partial class NavMenu : ComponentBase
     {
