0d06e2561b7a12fc074c68ee194c3f909fe0c7b8
Pier-Luc Bonneville <pier-luc.bonneville@priv.gc.ca>
2020-07-06 23:11:02 -0400

    Now saving the visitors to the access request.

--- src/Infrastructure/Services/Xrm/Entities/gc_accessrequest.cs
+++ src/Infrastructure/Services/Xrm/Entities/gc_accessrequest.cs
@@ -1,10 +1,23 @@
 ﻿using OfficeEntry.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace OfficeEntry.Infrastructure.Services.Xrm.Entities
 {
+    internal class gc_accessrequest_contact_visitor
+    {
+        [Key]
+        public Guid gc_accessrequest_contact_visitorid { get; set; }
+
+        public Guid gc_accessrequestid { get; set; }
+
+        public Guid contactid { get; set; }
+
+        //public Guid gc_accessrequest_contact_visitorid { get; set; }
+    }
+
     internal class gc_accessrequest
     {
         public string gc_name { get; set; }
@@ -18,9 +31,11 @@ namespace OfficeEntry.Infrastructure.Services.Xrm.Entities
         public contact gc_manager { get; set; }
         public AccessReasons gc_accessreason { get; set; }
         public DateTime gc_starttime { get; set; }
-        public IList<contact> gc_accessrequest_contact_visitors { get; set; }
-        public IList<gc_assetrequest> gc_accessrequest_assetrequest { get; set; }
+        //public IList<contact> gc_accessrequest_contact_visitors { get; set; } = new List<contact>();
+        public IList<gc_assetrequest> gc_accessrequest_assetrequest { get; set; } = new List<gc_assetrequest>();
         public int statecode { get; set; }
+
+        public IList<gc_accessrequest_contact_visitor> gc_accessrequest_contact_visitors { get; set; } = new List<gc_accessrequest_contact_visitor>();
 
         public static AccessRequest Convert(gc_accessrequest accessRequest)
         {
@@ -45,7 +60,24 @@ namespace OfficeEntry.Infrastructure.Services.Xrm.Entities
                     Key = (int)accessRequest.gc_approvalstatus,
                     Value = Enum.GetName(typeof(ApprovalStatus), accessRequest.gc_approvalstatus)
                 },
-                Visitors = accessRequest.gc_accessrequest_contact_visitors.Select(v => contact.Convert(v)).ToList()
+                //Visitors = accessRequest.gc_accessrequest_contact_visitors.Select(v => contact.Convert(v)).ToList()
+            };
+        }
+
+        public static gc_accessrequest MapFrom(AccessRequest accessRequest)
+        {
+            return new gc_accessrequest
+            {
+                gc_name = $"{accessRequest.Employee.FullName} - {accessRequest.StartTime:yyyy-MM-dd}",
+                gc_accessreason = (AccessReasons)accessRequest.Reason.Key,
+                gc_approvalstatus = ApprovalStatus.Pending,
+                gc_building = new gc_building { gc_buildingid = accessRequest.Building.Id },
+                gc_details = accessRequest.Details,
+                gc_employee = new contact { contactid = accessRequest.Employee.Id },
+                gc_endtime = accessRequest.EndTime,
+                gc_floor = new gc_floor { gc_floorid = accessRequest.Floor.Id },
+                gc_manager = new contact { contactid = accessRequest.Manager.Id },
+                gc_starttime = accessRequest.StartTime
             };
         }
     }
