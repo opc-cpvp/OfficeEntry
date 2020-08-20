1084ece1c84254a98ff6afe6a79180b4d0ecc3c0
Stéphanie Brouillard <Stephanie.Brouillard@priv.gc.ca>
2020-08-20 09:03:39 -0400

    Seperate datepicker logic from survey.js

---
+++ src/WebApp/wwwroot/js/datepicker.js
@@ -0,0 +1,30 @@
+﻿/* Canadian-French initialisation for the jQuery UI date picker plugin. */
+(function (factory) {
+	if (typeof define === "function" && define.amd) {
+
+		// AMD. Register as an anonymous module.
+		define(["../widgets/datepicker"], factory);
+	} else {
+
+		// Browser globals
+		factory(jQuery.datepicker);
+	}
+}(function (datepicker) {
+	fetch('/json/datepicker-fr.json')
+		.then(function (response) { return response.json(); })
+		.then(function (data) {
+			datepicker.regional["fr-CA"] = data;
+		});
+
+	datepicker.setDefaults(datepicker.regional["fr-CA"]);
+
+	return datepicker.regional["fr-CA"];
+
+}));
+
+var initializeDatepicker = function (locale) {
+	if (locale === 'fr') {
+		return $.datepicker.setDefaults($.datepicker.regional['fr-CA']);
+	}
+	return $.datepicker.setDefaults($.datepicker.regional['']);
+}
