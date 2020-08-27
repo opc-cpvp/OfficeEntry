32ae4c4c196f382e35396fc63c91c7468aae6c88
Stéphanie Brouillard <Stephanie.Brouillard@priv.gc.ca>
2020-08-27 14:10:32 -0400

    Added index page when usersettings are incomplete

---
+++ src/WebApp/Pages/TermsAndConditions.razor.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+
+namespace OfficeEntry.WebApp.Pages
+{
+    [Authorize]
+    public partial class TermsAndConditions : ComponentBase
+    {
+        [Inject] public NavigationManager NavigationManager { get; set; }
+        [Inject] public IMediator Mediator { get; set; }
+        [Inject] public IStringLocalizer<App> Localizer { get; set; }
+        [Inject] public ILocalStorageService LocalStorage { get; set; }
+
+        private bool? isHealthAndSafetyMeasuresAccepted = null;
+        private bool? isPrivacyActStatementAccepted = null;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender)
+                return;
+
+            isHealthAndSafetyMeasuresAccepted = await LocalStorage.GetItemAsync<bool>("isHealthAndSafetyMeasuresAccepted");
+            isPrivacyActStatementAccepted = await LocalStorage.GetItemAsync<bool>("isPrivacyActStatementAccepted");
+
+            StateHasChanged();
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+    }
+}
