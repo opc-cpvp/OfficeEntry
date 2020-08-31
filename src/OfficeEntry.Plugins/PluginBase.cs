a967d43f568d886318a2cc49d19a93ec5922cb2d
Joshua Campbell <joshcampbell191@users.noreply.github.com>
2020-08-31 13:42:27 -0400

    Features/accessrequest plugin (#43)
    Updated email templates with application URL using the new gc_guid field

---
+++ src/OfficeEntry.Plugins/PluginBase.cs
@@ -0,0 +1,57 @@
+using Microsoft.Xrm.Sdk;
+using System;
+using System.ServiceModel;
+
+namespace OfficeEntry.Plugins
+{
+    /*
+     * Plugin development guide: https://docs.microsoft.com/powerapps/developer/common-data-service/plug-ins
+     * Best practices and guidance: https://docs.microsoft.com/powerapps/developer/common-data-service/best-practices/business-logic/
+     */
+    public abstract class PluginBase : IPlugin
+    {
+        protected string PluginClassName { get; }
+
+        internal PluginBase(Type pluginClassName)
+        {
+            PluginClassName = pluginClassName.ToString();
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            if (serviceProvider == null)
+            {
+                throw new InvalidPluginExecutionException("serviceProvider");
+            }
+
+            var localPluginContext = new LocalPluginContext(serviceProvider);
+
+            localPluginContext.Trace($"Entered {PluginClassName}.Execute() " +
+                $"Correlation Id: {localPluginContext.PluginExecutionContext.CorrelationId}, " +
+                $"Initiating User: {localPluginContext.PluginExecutionContext.InitiatingUserId}");
+
+            try
+            {
+                ExecuteCdsPlugin(localPluginContext);
+
+                // Now exit - if the derived plugin has incorrectly registered overlapping event registrations, guard against multiple executions.
+                return;
+            }
+            catch (FaultException<OrganizationServiceFault> orgServiceFault)
+            {
+                localPluginContext.Trace($"Exception: {orgServiceFault.ToString()}");
+
+                throw new InvalidPluginExecutionException($"OrganizationServiceFault: {orgServiceFault.Message}", orgServiceFault);
+            }
+            finally
+            {
+                localPluginContext.Trace($"Exiting {PluginClassName}.Execute()");
+            }
+        }
+
+        protected virtual void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
+        {
+            // Do nothing.
+        }
+    }
+}
