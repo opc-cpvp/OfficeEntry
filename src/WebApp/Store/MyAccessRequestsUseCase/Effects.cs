3a2d81af02e51df0c7fb3c7525223770d4392094
Stéphanie Brouillard <Stephanie.Brouillard@priv.gc.ca>
2020-09-02 10:51:05 -0400

    Seperated classes in their respective files and changed FetchData naming to be more descriptive

---
+++ src/WebApp/Store/MyAccessRequestsUseCase/Effects.cs
@@ -0,0 +1,25 @@
+﻿using Fluxor;
+using MediatR;
+using OfficeEntry.Application.AccessRequests.Queries.GetAccessRequests;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficeEntry.WebApp.Store.MyAccessRequestsUseCase
+{
+    public class Effects
+    {
+        private readonly IMediator _mediator;
+
+        public Effects(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [EffectMethod]
+        public async Task HandleFetchDataAction(GetMyAccessRequestsAction action, IDispatcher dispatcher)
+        {
+            var accessRequests = (await _mediator.Send(new GetAccessRequestsQuery())).ToArray();
+            dispatcher.Dispatch(new GetMyAccessRequestsResultAction(accessRequests));
+        }
+    }
+}
---
+++ src/WebApp/Store/MyAccessRequestsUseCase/Reducers.cs
@@ -0,0 +1,17 @@
+﻿using Fluxor;
+using OfficeEntry.Domain.Entities;
+using System;
+
+namespace OfficeEntry.WebApp.Store.MyAccessRequestsUseCase
+{
+    public class Reducers
+    {
+        [ReducerMethod]
+        public static MyAccessRequestsState ReduceGetMyAccessRequestsAction(MyAccessRequestsState state, GetMyAccessRequestsAction action) =>
+            new MyAccessRequestsState(isLoading: true, accessRequests: Array.Empty<AccessRequest>());
+
+        [ReducerMethod]
+        public static MyAccessRequestsState ReduceGetMyAccessRequestsResultAction(MyAccessRequestsState state, GetMyAccessRequestsResultAction action) =>
+            new MyAccessRequestsState(isLoading: false, accessRequests: action.AccessRequests);
+    }
+}
