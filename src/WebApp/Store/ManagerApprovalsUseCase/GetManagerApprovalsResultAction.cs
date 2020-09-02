8f1a772d9b2817b6d53a09d82ea983b82ae91a84
Stéphanie Brouillard <Stephanie.Brouillard@priv.gc.ca>
2020-09-02 10:49:26 -0400

    Changed name of namespace and changed FetchData naming to be more descriptive

---
+++ src/WebApp/Store/ManagerApprovalsUseCase/GetManagerApprovalsResultAction.cs
@@ -0,0 +1,15 @@
+﻿using OfficeEntry.Domain.Entities;
+using System.Collections.Generic;
+
+namespace OfficeEntry.WebApp.Store.ManagerApprovalsUseCase
+{
+    public class GetManagerApprovalsResultAction
+    {
+        public IReadOnlyList<AccessRequest> AccessRequests { get; }
+
+        public GetManagerApprovalsResultAction(IReadOnlyList<AccessRequest> accessRequests)
+        {
+            AccessRequests = accessRequests;
+        }
+    }
+}
---
+++ src/WebApp/Store/ManagerApprovalsUseCase/ManagerApprovalsState.cs
@@ -0,0 +1,25 @@
+﻿using OfficeEntry.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OfficeEntry.WebApp.Store.ManagerApprovalsUseCase
+{
+    public class ManagerApprovalsState
+    {
+        public bool IsLoading { get; }
+
+        public IReadOnlyList<AccessRequest> AccessRequests { get; }
+
+        public int PendingApprovals { get; }
+
+        public ManagerApprovalsState(bool isLoading, IReadOnlyList<AccessRequest> accessRequests)
+        {
+            IsLoading = isLoading;
+
+            AccessRequests = accessRequests;
+
+            PendingApprovals = accessRequests
+                .Count(x => x.Status.Key == (int)AccessRequest.ApprovalStatus.Pending);
+        }
+    }
+}
