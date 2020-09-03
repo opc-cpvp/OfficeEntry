a5059265a6835f3d71c59c7ca19c243c84df4f0c
Pier-Luc Bonneville <pier-luc.bonneville@priv.gc.ca>
2020-09-03 10:23:48 -0400

    URL based navigation

--- src/WebApp/Pages/Index.razor.cs
+++ src/WebApp/Pages/Index.razor.cs
@@ -15,99 +15,34 @@ namespace OfficeEntry.WebApp.Pages
     public partial class Index : ComponentBase
     {
         [Inject] public NavigationManager NavigationManager { get; set; }
-        [Inject] public IMediator Mediator { get; set; }
-        [Inject] public ILocalStorageService LocalStorage { get; set; }
-        [Inject] public IStringLocalizer<App> Localizer { get; set; } 
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
+        protected override void OnInitialized()
         {
-            if (!firstRender)
-                return;
-
-            var currentPageUrl = NavigationManager.Uri.Replace(NavigationManager.BaseUri, "");
-
-            if (currentPageUrl == "entree-splash")
-                return;
-
-            var currentCulture = await LocalStorage.GetItemAsync<string>("BlazorCulture");
-
-            if (currentCulture is null)
-                return;
-
-            var culture = currentCulture;
-            var localizedLandingPage = await GetLocalizedLandingPage(culture);
-            var uri = new Uri(new Uri(NavigationManager.BaseUri), localizedLandingPage)
-                .GetComponents(UriComponents.PathAndQuery, UriFormat.Unescaped);
-            var query = $"?culture={Uri.EscapeDataString(culture)}&" +
-                $"redirectUri={Uri.EscapeDataString(uri)}";
-
-            NavigationManager.NavigateTo("/Culture/SetCulture" + query, forceLoad: true);
-
-            await base.OnAfterRenderAsync(firstRender);
-        }
-
-        public async Task SetLanguageToFrench()
-        {
-            await LocalStorage.SetItemAsync("BlazorCulture", "fr-CA");
-
-            NavigationManager.NavigateTo("/", forceLoad: true);
-        }
-
-        public async Task SetLanguageToEnglish()
-        {
-            await LocalStorage.SetItemAsync("BlazorCulture", "en-CA");
-
-            NavigationManager.NavigateTo("/", forceLoad: true);
-        }
-
-        private async Task<bool> HasAcceptedTermsAndConditions()
-        {
-
-            var isPrivacyActStatementAccepted = await GetPrivacyActStatement();
-            var isHealthAndSafetyMeasuresAccepted = await GetHealthAndSafetyMeasures();
-
-            return (isHealthAndSafetyMeasuresAccepted && isPrivacyActStatementAccepted);
-
-            async Task<bool> GetPrivacyActStatement()
+            var baseUriRange = NavigationManager.BaseUri.Length..;
+            var uri = NavigationManager.Uri[baseUriRange];
+            switch (uri.Trim('/'))
             {
-                if (!(await LocalStorage.ContainKeyAsync("isPrivacyActStatementAccepted")))
-                {
-                    if(await LocalStorage.GetItemAsync<bool>("isPrivacyActStatementAccepted") == false)
-                    {
-                        var isPrivacyActStatementAccepted = await Mediator.Send(new GetPrivacyStatementForCurrentUserQuery());
-                        await LocalStorage.SetItemAsync("isPrivacyActStatementAccepted", isPrivacyActStatementAccepted);
-                    }                
-                }
-
-                return await LocalStorage.GetItemAsync<bool>("isPrivacyActStatementAccepted");
+                case "en":
+                    SetLanguageToEnglish();
+                    break;
+                case "fr":
+                    SetLanguageToFrench();
+                    break;
+                default:
+                    break;
             }
 
-            async Task<bool> GetHealthAndSafetyMeasures()
-            {
-                if (!(await LocalStorage.ContainKeyAsync("isHealthAndSafetyMeasuresAccepted")))
-                {
-                    if (await LocalStorage.GetItemAsync<bool>("isHealthAndSafetyMeasuresAccepted") == false)
-                    {
-                        var isHealthAndSafetyMeasuresAccepted = await Mediator.Send(new GetHealthAndSafetyMeasuresForCurrentUserQuery());
-                        await LocalStorage.SetItemAsync("isHealthAndSafetyMeasuresAccepted", isHealthAndSafetyMeasuresAccepted);
-                    }
-                }
-
-                return await LocalStorage.GetItemAsync<bool>("isHealthAndSafetyMeasuresAccepted");
-            }
+            base.OnInitialized();
         }
 
-        private async Task<string> GetLocalizedLandingPage(string culture)
+        public void SetLanguageToFrench()
         {
-            var hasAcceptedTermsAndConditions = await HasAcceptedTermsAndConditions();
+            NavigationManager.NavigateTo("/fr/mes-demandes-d-acces", forceLoad: true);
+        }
 
-            CultureInfo.CurrentUICulture = new CultureInfo(culture);
-
-            if (!hasAcceptedTermsAndConditions)
-            {               
-                return Localizer.GetString("terms-and-conditions");
-            }
-            return Localizer.GetString("access-requests");
+        public void SetLanguageToEnglish()
+        {
+            NavigationManager.NavigateTo("/en/my-access-requests", forceLoad: true);
         }
     }
 }
