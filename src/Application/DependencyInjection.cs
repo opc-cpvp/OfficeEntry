785060c72bd8da02e95af39df107a47209ce12f9
Pier-Luc Bonneville <pier-luc.bonneville@priv.gc.ca>
2022-03-16 18:47:25 -0400

    Added Polly and a retry behavior for disposed  System.Net.Http.HttpClient

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -20,6 +20,7 @@ namespace OfficeEntry.Application
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RetryBehavior<,>));
 
             return services;
         }
