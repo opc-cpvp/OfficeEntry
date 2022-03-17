738238dd4aaa1703daea39c12cad440dafffed7e
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2022-03-17 09:19:19 -0400

    Features/polly (#501)
    * Added Polly and a retry behavior for disposed  System.Net.Http.HttpClient
    
    * Fixed the encoding of the file
    
    Co-authored-by: Pier-Luc Bonneville <pier-luc.bonneville@priv.gc.ca>

---
+++ src/Application/Common/Behaviours/RetryBehavior.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Polly;
+
+namespace OfficeEntry.Application.Common.Behaviours;
+
+public class RetryBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<TRequest> _logger;
+    private readonly IAsyncPolicy _retryPolicy;
+
+    public RetryBehavior(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+
+        _retryPolicy = Policy
+            .Handle<ObjectDisposedException>(e => e.ObjectName is "System.Net.Http.HttpClient")
+            .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(i), (exception, timeSpan) =>
+            {
+                // Logic to be executed before each retry, such as logging
+                _logger.LogError(exception, "Waiting {timeSpan} seconds before retrying.", timeSpan);
+            });
+    }
+
+    public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        return _retryPolicy.ExecuteAsync(() => next());
+    }
+}
