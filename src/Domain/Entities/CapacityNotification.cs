3513161d642c51e04a138908160f3dcbec627231
Stéphanie Brouillard <Stephanie.Brouillard@priv.gc.ca>
2023-04-27 06:10:58 -0400

    Add notification type to Capacity notification

--- src/Domain/Entities/CapacityNotification.cs
+++ src/Domain/Entities/CapacityNotification.cs
@@ -2,12 +2,31 @@
 {
     public class CapacityNotification : EmailTemplate
     {
-        public override string Title => "Entrée au bureau - La capacité maximale a été atteinte ou dépassée / Office Entry - Capacity has been reached or exceeded";
         public int Capacity { get; set; }
         public DateTime Date { get; set; }
         public string RoleEnglishName { get; set; }
         public string RoleFrenchName { get; set; }
         public Building Building { get; set; }
         public Floor Floor { get; set; }
+        public NotificationType Type { get; init; }
+        public override string Title => GetTitle(Type);
+
+        public CapacityNotification(NotificationType notificationType)
+        {
+            Type = notificationType;
+        }
+        public enum NotificationType
+        {
+            Available = 1,
+            Maximum = 2
+        }
+
+        private string GetTitle(NotificationType type)
+        => type switch
+        {
+            NotificationType.Available => $"Entrée au bureau - Un {RoleFrenchName} n'est plus requis / Office Entry - A {RoleEnglishName} is no longer required",
+            NotificationType.Maximum => "Entrée au bureau - La capacité maximale a été atteinte ou dépassée / Office Entry - Capacity has been reached or exceeded",
+            _ => null
+        };
     }
 }
